b328231926abcf26159823c36cbf3d903ec59fd6
amantesf <tesfayee.amanuel@gmail.com>
2025-02-16 15:04:38 -0500

    Add dictionairy

---
+++ hangman.py
@@ -0,0 +1,7 @@
+import random
+
+print("Welcome to hangman")
+print("-------------------------------------------")
+
+wordDictionary = ["sunflower", "house", "diamond", "memes","yeet","hello", "howdy", "like", "subscribe"]
+
