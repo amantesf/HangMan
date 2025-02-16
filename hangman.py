6a96f925b00c040af9ba50b7cb7dd3a48c84b12a
amantesf <tesfayee.amanuel@gmail.com>
2025-02-16 15:12:38 -0500

    Update dictionairy

--- hangman.py
+++ hangman.py
@@ -3,5 +3,5 @@ import random
 print("Welcome to hangman")
 print("-------------------------------------------")
 
-wordDictionary = ["sunflower", "house", "diamond", "memes","yeet","hello", "howdy", "like", "subscribe"]
+wordDictionary = ["Apple", "Bicycle", "Chandelier", "Detonate","Emergency","Flower", "Graveyard", "Handbrake", "Institute", "Juxtapose", "Klondike", "Lemonade", "Microwave", "Napoleon", "Officer", "Patagonia", "Queen", "Rotisserie", "Stockholm", "Troublesome", "Universe", "Volcano", "Watermelon", "Xylophone", "Yellow", "Zimbabwe"]
 
