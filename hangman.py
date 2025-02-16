f074e41d56f73c59687b04b57547aec923c5f56e
amantesf <tesfayee.amanuel@gmail.com>
2025-02-16 15:15:45 -0500

    Add lines and hangman

--- hangman.py
+++ hangman.py
@@ -5,3 +5,68 @@ print("-------------------------------------------")
 
 wordDictionary = ["Apple", "Bicycle", "Chandelier", "Detonate","Emergency","Flower", "Graveyard", "Handbrake", "Institute", "Juxtapose", "Klondike", "Lemonade", "Microwave", "Napoleon", "Officer", "Patagonia", "Queen", "Rotisserie", "Stockholm", "Troublesome", "Universe", "Volcano", "Watermelon", "Xylophone", "Yellow", "Zimbabwe"]
 
+randomWord = random.choice(wordDictionary)
+
+for x in randomWord:
+  print("_", end=" ")
+
+def print_hangman(wrong):
+  if(wrong == 0):
+    print("\n+---+")
+    print("    |")
+    print("    |")
+    print("    |")
+    print("   ===")
+  elif(wrong == 1): 
+    print("\n+---+")
+    print("O   |")
+    print("    |")
+    print("    |")
+    print("   ===")
+  elif(wrong == 2):
+    print("\n+---+")
+    print("O   |")
+    print("|   |")
+    print("    |")
+    print("   ===")
+  elif(wrong == 3):
+    print("\n+---+")
+    print(" O  |")
+    print("/|  |")
+    print("    |")
+    print("   ===")
+  elif(wrong == 4):
+    print("\n+---+")
+    print(" O  |")
+    print("/|\ |")
+    print("    |")
+    print("   ===")
+  elif(wrong == 5):
+    print("\n+---+")
+    print(" O  |")
+    print("/|\ |")
+    print("/   |")
+    print("   ===")
+  elif(wrong == 6):
+    print("\n+---+")
+    print(" O   |")
+    print("/|\  |")
+    print("/ \  |")
+    print("    ===")
+
+def printWord(guessedLetters):
+  counter=0
+  rightLetters=0
+  for char in randomWord:
+    if(char in guessedLetters):
+      print(randomWord[counter], end=" ")
+      rightLetters+=1
+    else:
+      print(" ", end=" ")
+    counter+=1
+  return rightLetters
+
+def printLines():
+  print("\r")
+  for char in randomWord:
+    print("\u203E", end=" ")
